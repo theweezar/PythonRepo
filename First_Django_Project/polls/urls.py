1d8e972abf3550fcbe94a894849b0abb2d324d25
minhduc <minhducducminh1999@gmail.com>
2019-08-14 09:32:37 +0700

    changed sth

--- First_Django_Project/polls/urls.py
+++ First_Django_Project/polls/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
   # bên trong views. có chứa những phương thức def ... mà ta đã viết ở polls/urls.py
   path('',views.index,name='index')
-  # path('',views.another,name='another')
+  # path('another',views.another,name='another')
 ]
