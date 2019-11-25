8e51d9fbb1191119ef33a1103f4287610c6577b8
minhduc <minhducducminh1999@gmail.com>
2019-11-25 18:02:49 +0700

    commited

---
+++ BasicDeepLearning/LinearRegression.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("data_linear.csv") 
+# data ở đây là 1 class Object, không phải là list
+# data.values sẽ return lại 1 Array gôm các phần tử trong đó theo dạng [][]
+print("App is running")
+
+N = data.shape[0]
+x = data[:, 0].reshape(-1, 1)
+y = data[:, 1].reshape(-1, 1)
+plt.scatter(x, y)
+plt.xlabel('mét vuông')
+plt.ylabel('giá')
+
+x = np.hstack((np.ones((N, 1)), x))
+
+w = np.array([0.,1.]).reshape(-1,1)
+
+numOfIteration = 100
+cost = np.zeros((numOfIteration,1))
+learning_rate = 0.000001
+for i in range(1, numOfIteration):
+    r = np.dot(x, w) - y
+    cost[i] = 0.5*np.sum(r*r)
+    w[0] -= learning_rate*np.sum(r)
+    # correct the shape dimension
+    w[1] -= learning_rate*np.sum(np.multiply(r, x[:,1].reshape(-1,1)))
+    print(cost[i])
+predict = np.dot(x, w)
+plt.plot((x[0][1], x[N-1][1]),(predict[0], predict[N-1]), 'r')
+plt.show()
+
+x1 = 50
+y1 = w[0] + w[1] * 50
+print('Giá nhà cho 50m^2 là : ', y1)
+
+input()
