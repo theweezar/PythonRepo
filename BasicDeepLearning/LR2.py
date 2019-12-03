90a842ee36d144b093722d5035bef30398d2c251
minhduc <minhducducminh1999@gmail.com>
2019-12-03 12:31:04 +0700

    committed

--- BasicDeepLearning/LR2.py
+++ BasicDeepLearning/LR2.py
@@ -26,40 +26,47 @@ print(f"y: \n{y}\n")
 print(f"w[w0,w1,w2]: {w}\n")
 
 learning_rate = 0.01
-iTerate = 200
+iTerate = 1000
 
 for i in range(0,iTerate):
   # lấy w[0] + x1*w[1] + x2*w[2] rồi sau đó cộng lại hết với nhau theo từng hàng, rồi bỏ
   # vào hàm sigmoid để yP dc trả về là 1 ma trận
   yP = sigmoid(np.sum(w * x,axis=1).reshape(-1,1))
-  print(f"yP : \n{yP}\n")
+  # print(f"yP : \n{yP}\n")
 
-  loss = (-1 / N) * np.sum(y * np.log10(yP) + (1 - y) * np.log10(1 - yP))
+  loss = -np.sum(y * np.log(yP) + (1 - y) * np.log(1 - yP)) / N
   
-  print(f"Loss : {loss}\n")
+  print(f"Loss : {loss}")
 
-  w[0] -= (-1 / (N * math.log(10,math.e)) * np.sum(y - yP)) * learning_rate
-  w[1] -= (-1 / (N * math.log(10,math.e)) * np.sum(x[:,1] * (y - yP))) * learning_rate
-  w[2] -= (-1 / (N * math.log(10,math.e)) * np.sum(x[:,2] * (y - yP))) * learning_rate
+  r = y - yP
+  # print(f"r: \n{r}")
+  w[0] -= - np.sum(r) * learning_rate 
+  w[1] -= - np.sum(x[:,1].reshape((-1,1)) * (r)) * learning_rate
+  w[2] -= - np.sum(x[:,2].reshape((-1,1)) * (r)) * learning_rate
   
-
-  
+# w *= -1
 print(f"w after: {w}\n")
 
-salary = 100
-hour = 100
+salary = 4
+hour = 3
 
 yP = 1 / (1 + math.exp(-(w[0] + w[1] * salary + w[2] * hour)))
 
 print(f"\nPredited: {yP}\n")
+# quét hết 1 cột data, cột nào có pass = 1 thì gán vào xAllowed và ngược lại thì gán vào xDenied
+xAllowed = data.values[:,0:3][data.values[:,2] == 1] 
+xDenied = data.values[:,0:3][data.values[:,2] == 0]
 
-# plt.scatter(x[:,1],x[:,2])
-# plt.xlabel('Lương (triệu)')
-# plt.ylabel('Kinh nghiệm (năm)')
+plt.scatter(xAllowed[:,0],xAllowed[:,1],color='red',label='Allowed')
+plt.scatter(xDenied[:,0],xDenied[:,1],color='blue',label='Denied')
+plt.legend(loc=1)
 
-# t = 0.8
-# plt.plot((4, 10),(-(w[0]+4*w[1]+ np.log(1/t-1))/w[2], -(w[0] + 10*w[1]+ np.log(1/t-1))/w[2]), 'g')
+plt.xlabel('Lương (triệu)')
+plt.ylabel('Kinh nghiệm (năm)')
 
-# plt.show()
+t = 0.5
+plt.plot((4, 10),(-(w[0]+4*w[1]+ np.log(1/t-1)) / w[2], -(w[0] + 10*w[1]+ np.log(1/t-1)) / w[2]), 'g')
+
+plt.show()
 
 input()
