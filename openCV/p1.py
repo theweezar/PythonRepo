777858955e55deca570719478d74236838fee3ff
minhduc <minhducducminh1999@gmail.com>
2019-12-24 18:13:30 +0700

    committed

--- openCV/p1.py
+++ openCV/p1.py
@@ -1,26 +1,31 @@
 import cv2 as cv
+import os
 import numpy as np
 
-img = cv.imread('pic.jpg')
-print(f"First img data: \n{img}")
-height = len(img)
-width = len(img[0])
+class ImageProcessing:
+  def __init__(self ,imgLink):
+    self.img = cv.imread(imgLink)
+    self.height = len(self.img)
+    self.width = len(self.img[0])
+    self.r = np.array(self.img[:,:,0])
+    self.g = np.array(self.img[:,:,1])
+    self.b = np.array(self.img[:,:,2])
 
-r = np.array(img[:,:,0])
-g = np.array(img[:,:,1])
-b = np.array(img[:,:,2])
+  def toBlackWhite(self):
+    t = self.r * 0.299 + self.g * 0.587 + self.b * 0.114
+    self.r = self.g = self.b = t
+  
+  def toYellow(self):
+    self.b[:,:] = 255
+  
+  def saveImage(self ,imgName = ''):
+    afterImg = np.hstack((self.r.reshape((-1,1)),self.g.reshape((-1,1)),self.b.reshape((-1,1))))
+    afterImg = afterImg.reshape((self.height,self.width,3))
+    # print(f"After img data: \n{afterImg}\n")
+    cv.imwrite(imgName, afterImg)
+    
 
-# r[:,0] = r[:,-1]
-# print(r[0,:])
-
-afterImg = np.hstack((r.reshape((-1,1)),g.reshape((-1,1)),b.reshape((-1,1))))
-print(f"After img data: \n{afterImg}\n")
-print(afterImg[:width])
-# afterImg = afterImg.reshape((width,height))
-# img = img.T
-# print(img)
-# cv.imwrite('pic_rw.jpg', img)
-
-# a = np.array([[1,2,3],[4,5,6],[7,8,9]])
-# print(a)
-# print(a.T)
+img = ImageProcessing('pic.jpg')
+# img.toBlackWhite()
+img.toYellow()
+img.saveImage('pic_rw.jpg')
