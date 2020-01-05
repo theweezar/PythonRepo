34bc2da85c8e3b06df50cf95893e28bb8e8984ab
minhduc <minhducducminh1999@gmail.com>
2020-01-05 20:26:58 +0700

    committed

--- openCV/p2.py
+++ openCV/p2.py
@@ -1,10 +1,51 @@
-# Ở đây, mình sẽ minh họa từng bước xử lý ảnh trong deep learning
-# để sau này đọc lại có thể hiểu được cách xử lý ảnh trong DL ra sao
+
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt 
 
-# 1: Load ảnh
+class DetectSomething:
+  def __init__(self):
+    self.cameraOn()
 
-img = cv.imread('pic.jpg')
-print(img)
+  def toBlackWhite(self,frame):
+    r,g,b = cv.split(frame)
+    t = r * 0.299 + g * 0.587 + b * 0.114
+    return cv.merge((t,t,t))
+
+  def edgeDetection(self,frame):
+    k = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
+    return cv.filter2D(frame,0,k)
+
+  def cameraOn(self):
+    # frame shape [480,640,3]
+    cap = cv.VideoCapture(0)
+    isOn = True
+    fx = fy = 0
+    tx = ty = 0
+    if not cap.isOpened:
+      isOn = False
+      print("Can't opened camera")
+    else:
+      ret, frame = cap.read()
+      if not ret:
+        isOn = False
+        print("Something went wrong, can't receive frames !")
+      else:
+        fy = int(frame.shape[0] / 4)
+        fx = int(frame.shape[1] / 4)
+        ty = int(frame.shape[0] - fy)
+        tx = int(frame.shape[1] - fx)
+    while isOn:
+      ret, frame = cap.read()
+      if not ret:
+        print("Something went wrong, can't receive frames !")
+        break
+      pFrame = frame[fy:ty,fx:tx]
+      # pFrame = self.toBlackWhite(pFrame)
+      pFrame = self.edgeDetection(pFrame)
+      frame[fy:ty,fx:tx] = pFrame
+      cv.imshow('frame',frame)
+      if cv.waitKey() == 27:
+        break
+
+d = DetectSomething()
