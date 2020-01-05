bbe0a0318b0519a4e582275c76f875a8e69d6fef
minhduc <minhducducminh1999@gmail.com>
2020-01-05 23:10:26 +0700

    committed

--- openCV/p2.py
+++ openCV/p2.py
@@ -1,7 +1,8 @@
 
 import numpy as np
 import cv2 as cv
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import os
 
 class DetectSomething:
   def __init__(self):
@@ -22,6 +23,7 @@ class DetectSomething:
     isOn = True
     fx = fy = 0
     tx = ty = 0
+    # ==================================
     if not cap.isOpened:
       isOn = False
       print("Can't opened camera")
@@ -35,17 +37,31 @@ class DetectSomething:
         fx = int(frame.shape[1] / 4)
         ty = int(frame.shape[0] - fy)
         tx = int(frame.shape[1] - fx)
+    # ==================================
+    a = 0
+    path = "hand"
+    if not os.path.exists(path):
+      os.mkdir(path)
+    else:
+      a = len(os.listdir(path))
+      print(a)
+    # ==================================
     while isOn:
       ret, frame = cap.read()
       if not ret:
         print("Something went wrong, can't receive frames !")
         break
+      cv.rectangle(frame,(fx,fy),(tx,ty),(255,0,0))
       pFrame = frame[fy:ty,fx:tx]
       # pFrame = self.toBlackWhite(pFrame)
-      pFrame = self.edgeDetection(pFrame)
-      frame[fy:ty,fx:tx] = pFrame
+      # pFrame = self.edgeDetection(pFrame)
+      # frame[fy:ty,fx:tx] = pFrame
       cv.imshow('frame',frame)
       if cv.waitKey() == 27:
         break
+      elif cv.waitKey() == 32:
+        cv.imwrite(f"hand\\hand{a}.jpg",pFrame)
+        a += 1
+
 
 d = DetectSomething()
