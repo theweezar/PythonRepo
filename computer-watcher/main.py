2fbe56eee1407de551a9d0275bac9fad8c5d4585
minhduc <minhducducminh1999@gmail.com>
2020-12-23 12:48:46 +0700

    committed

--- computer-watcher/main.py
+++ computer-watcher/main.py
@@ -1,16 +1,58 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import numpy as np
+import cv2
+import time
+import base64
+from PIL import Image
+from io import BytesIO
 app = Flask(__name__)
 socketio = SocketIO(app) 
+fps = 1/60
+app.config['streaming'] = False
 
 @app.route('/')
 def hello_world():
+  # app.logger.info('Conntection is established')
   return render_template("index.html")
 
-@socketio.on('request_to_connect')
-def request_to_connect():
-  print("shit")
-  emit('connected_to_server', 'connection is established')
+@socketio.on('connect')
+def connect():
+  app.logger.info('Conntection is established')
+
+@socketio.on('start_webcam')
+def start_webcam():
+  cap = cv2.VideoCapture(0)
+  app.config['streaming'] = True
+  # Kiểm tra camera
+  if not cap.isOpened:
+    app.config['streaming'] = False
+    emit('error_camera_not_open')
+  else:
+    ret, frame = cap.read()
+    if not ret:
+      app.config['streaming'] = False
+      emit('cant_get_frame')
+  while app.config['streaming']:
+    ret, frame = cap.read()
+    if not ret:
+      emit('cant_get_frame')
+      break
+    # ret, buffer = cv2.imencode('.jpg', frame)
+    # array_buffer = base64.b64encode(buffer.tobytes())
+    frame = frame[:,::-1]
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    pil_img = Image.fromarray(frame)
+    buff = BytesIO()
+    pil_img.save(buff, format="JPEG")
+    new_image_string = base64.b64encode(buff.getvalue()).decode("utf-8")
+    emit('recording', new_image_string)
+    time.sleep(fps)
+
+@socketio.on('stop_webcam')
+def stop_webcam():
+  app.config['streaming'] = False
 
 if __name__ == "__main__":
-  app.run(port = 5005, debug = True)
+  # Phải set host = '0.0.0.0' thì mới có thể public trong network được
+  socketio.run(app, port=5000, debug=True, host='192.168.1.2')
--- computer-watcher/static/js/main.js
+++ computer-watcher/static/js/main.js
@@ -1,18 +1,38 @@
 window.onload = function(){
   const screen = document.getElementById("screenCpt")
   const webcam = document.getElementById("webcamCpt")
+  const frame = document.getElementById("frame")
+  const stop = document.getElementById("stopBtn")
   const socket = io()
   
   webcam.addEventListener("click", function(){
+    alert("Camera is starting...")
     socket.emit("start_webcam")
+    stop.classList.toggle("d-none")
   })
 
-  socket.emit("request_to_connect")
-  socket.on("connected_to_server", function(res){
-    console.log(res);
+  stop.addEventListener("click", function(){
+    alert("Stop....")
+    socket.emit("stop_webcam")
+    stop.classList.toggle("d-none")
   })
 
-  // socket.on("recording", function(p){
-  //   console.log(p);
-  // })
+  socket.on("recording", function(arrayBuffer){
+    const base64_img = "data:image/jpeg;base64,"+arrayBuffer
+    const img = new Image(400, 300)
+    img.src = base64_img
+    img.style.border = "2px solid black"
+    // console.log(base64_img);
+    frame.innerHTML = img.outerHTML
+  })
+
+  socket.on("connect", function(){
+    console.log("Connection is established")
+    alert("Connection is established")
+  })
+
+  socket.on("disconnect", function(){
+    this.close()
+  })
 }
+
