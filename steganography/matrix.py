0f8d4a264cbfb82b8b95f0ef7e076918001a3288
minhduc <minhducducminh1999@gmail.com>
2021-04-07 10:07:03 +0700

    committed

--- steganography/matrix.py
+++ steganography/matrix.py
@@ -2,22 +2,32 @@ import numpy as np
 
 # block
 fi = np.array([
-  [1,1,0,0,1],
-  [0,1,0,1,1],
-  [0,0,1,0,0],
-  [0,0,1,1,1],
-  [1,0,0,0,1]
+  [1,1,0,0,1,0],
+  [0,1,0,1,1,0],
+  [0,0,1,0,0,1],
+  [0,0,1,1,1,0],
+  [1,0,0,0,1,1],
+  [0,0,1,1,0,1]
 ])
 
 # key
 key = np.array([
-  [0,0,0,1,1],
-  [1,1,1,0,1],
-  [0,0,0,0,1],
-  [0,0,0,1,0],
-  [1,1,0,1,0]
+  [0,1,0],
+  [1,0,1],
+  [1,1,0],
 ])
 
-print('key:\n',key,'\nSum:',key.sum())
-print('fi & key:\n',np.bitwise_and(fi, key),'\nSum:',np.bitwise_and(fi, key).sum())
-print('fi ^ key:\n',np.bitwise_xor(fi, key),'\nSum:',np.bitwise_xor(fi, key).sum())
+print(fi)
+
+# print('key:\n',key,'\nSum:',key.sum())
+# print('fi & key:\n',np.bitwise_and(fi, key),'\nSum:',np.bitwise_and(fi, key).sum())
+# print('fi ^ key:\n',np.bitwise_xor(fi, key),'\nSum:',np.bitwise_xor(fi, key).sum())
+
+# dect_key_1 = np.ones((5,5),dtype=int)
+
+# dect_key_2 = np.zeros((5,5),dtype=int)
+
+# print(dect_key_1)
+
+# print('fi ^ dect_key_1:\n',np.bitwise_xor(fi, dect_key_1))
+# print('fi ^ dect_key_2:\n',np.bitwise_xor(fi, dect_key_2))
