0b18066a5222df1c5406ef6e0456eece5d9022ee
DucHoang <duc.hoang@globee.hk>
2021-10-10 00:00:48 +0700

    Ok

--- DoAnThayThua_EncryptImageRSA/server/App.py
+++ DoAnThayThua_EncryptImageRSA/server/App.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(
+        host='127.0.0.1',
+        port=3000,
+        debug=True
+    )
